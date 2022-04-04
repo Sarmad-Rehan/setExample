69edeeb7411ec1e9992758caf1de5afc768dac56
Sarmad Rehan <sarmadrehan52@gmail.com>
2022-04-04 09:59:12 +0500

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  const a = {1, 3};
+  const b = {3, 5};
+
+  final c = a.difference(b);
+  final d = b.difference(a);
+  final result = c.union(d);
+  print(result);
+  final newResult = result.reduce((value, element) => value + element);
+  print(newResult);
+}
